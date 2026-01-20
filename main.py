2c971e281bf737dddcf40d594b654b0f19465c1a
Artem DUBROVIN <RU20097672@domainru.ru.auchan.com>
2026-01-20 23:54:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import chess
 from chess_utils import print_board
+from chess_game import illegal_moves
 
 
 def main():
     board = chess.Board()
     print(print_board(board))
-
 
 if __name__ == "__main__":
     main()
